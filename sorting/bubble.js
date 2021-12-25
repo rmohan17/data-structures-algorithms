48244400d3043ee2dfc209c786e0f2904f3368fc
Radha Mohan <65498856+rmohan17@users.noreply.github.com>
2021-12-24 22:02:19 -0500

    Create bubble.js

---
+++ sorting/bubble.js
@@ -0,0 +1,17 @@
+const array = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      //swap
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+console.log(array)
+}
+bubbleSort(array);
